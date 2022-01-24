46a4b7701a4c529d6095f40434d557c5ed95f518
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2022-01-24 17:46:08 +0200

    Add files via upload

---
+++ Object/ObjectTask6.js
@@ -0,0 +1,32 @@
+function objectDemo (array){
+
+    let result = array.sort((a,b) => a.localeCompare(b));
+
+    let current = [];
+    let char = '';
+
+    for (let i=0 ; i < array.length ; i++){
+        current = result[i].split(' : ');
+        if (result[i][0]!==char){
+            console.log(result[i][0]);
+            char = result[i][0];
+        }
+        console.log(`  ${current[0]}: ${current[1]}`);
+    }
+}
+
+objectDemo(['Appricot : 20.4',
+
+    'Fridge : 1500',
+
+    'TV : 1499',
+
+    'Deodorant : 10',
+
+    'Boiler : 300',
+
+    'Apple : 1.25',
+
+    'Anti-Bug Spray : 15',
+
+    'T-Shirt : 10']);
