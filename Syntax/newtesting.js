1cea1f917eca696dc2f4edf4104a98b82b27d9a0
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2022-01-29 22:12:16 +0200

    Add files via upload

---
+++ Syntax/newtesting.js
@@ -0,0 +1,12 @@
+function calculate (str , num , dec){
+    let fruitType = str;
+    let fruitWeight = num;
+    let pricePerKilo = dec;
+
+    let fruitWeightKilo = fruitWeight / 1000;
+    let price = fruitWeightKilo * pricePerKilo;
+
+    console.log(`I need $${price.toFixed(2)} to buy ${fruitWeightKilo.toFixed(2)} kilograms ${fruitType}.`);
+}
+
+calculate('orange', 2500, 1.80);
