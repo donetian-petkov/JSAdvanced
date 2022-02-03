e38bb3fd33d077d2eeb9fc9a9599963bf189d6bd
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2022-02-03 11:47:13 +0200

    Add files via upload

--- AdvancedFunctions/Task1.js
+++ AdvancedFunctions/Task1.js
@@ -1,1 +1,12 @@
+function sortArray (array, order) {
 
+    if (order === 'asc') {
+        array.sort((a,b) => a-b);
+    } else if (order === 'desc'){
+        array.sort((a,b) => b-a);
+    }
+
+    return array;
+}
+
+sortArray([14, 7, 17, 6, 8], 'asc');
